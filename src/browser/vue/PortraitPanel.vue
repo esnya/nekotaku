862828172b5eddcf5cd59d2018a1a03316f9288d
ukatama <dev.ukatama@gmail.com>
2017-09-06 02:02:28 +0900

    Fix portrait view

--- src/browser/vue/PortraitPanel.vue
+++ src/browser/vue/PortraitPanel.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
-  div.portrait-panel
-    div.portrait(
-      v-for="style in styles.reverse()"
-      :style="style"
-      :data-visible="open"
-      @click="open = !open"
-    )
-    div.portrait-open(:data-visible="!open", @click="open = !open")
-      v-btn.primary(fab, dark)
-        v-icon mdi-arrow-expand-left
+  div
+    transition(name="portrait-slide")
+      div.portrait-panel(v-if="open", @click="open = !open")
+        div.portrait(
+          v-for="style in styles"
+          :style="style"
+        )
+    transition(name="portrait-slide")
+      div.btn-panel(v-if="!open", @click="open = !open")
+        v-btn.expand-btn.primary(fab, dark)
+          v-icon mdi-arrow-expand-left
 </template>
 
 <script>
@@ -24,6 +25,7 @@ export default {
         backgroundImage: `url(${url})`,
         transform: `translate(${i * 40}px, ${i * 10}px)`,
         opacity: i === 0 ? 1.0 : 0.5,
+        zIndex: this.portraits.length - i,
       }));
     },
   },
@@ -36,30 +38,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .portrait-panel
-    position: relative;
+.portrait-panel
+  position: relative;
+  z-index: 0;
 
-    > *
-      transition: transform 0.4s ease-in-out;
-      transform: translate(100%, 0);
+  .portrait
+    position: absolute;
+    bottom: 0px;
+    right: 0;
+    height: 250px;
+    width: 50%;
+    background-position: bottom right;
+    background-size: contain;
+    transition: all 0.4s ease-in-out;
 
-    > [data-visible=true]
-      transform: translate(0, 0);
 
-    .portrait
-      position: absolute;
-      bottom: 0px;
-      right: 0;
-      height: 250px;
-      width: 50%;
-      background-position: bottom right;
-      background-size: contain;
-      transition: all 0.4s ease-in-out;
+.btn-panel
+  position: relative;
+  height: 0;
 
-    .portrait-open
-      position: absolute;
-      bottom: 16px;
-      right: -20px;
-    
+  .expand-btn
+    position: absolute;
+    bottom: 16px;
+    right: -20px;
 
+.portrait-slide-enter-active, .portrait-slide-leave-active
+  transition: transform 0.4s ease-in-out;
+.portrait-slide-enter, .portrait-slide-leave-to
+  transform: translate(100%, 0);
 </style>
