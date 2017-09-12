e288e46a4476f79bced8025229237da4685bd0c3
ukatama <dev.ukatama@gmail.com>
2017-09-13 06:34:20 +0900

    Add welcome message

--- src/browser/vue/App.vue
+++ src/browser/vue/App.vue
@@ -3,4 +3,16 @@
     v-app
       transition(name="neko-slide")
         router-view
+      welcome-message
 </template>
+
+<script>
+import WelcomeMessage from './WelcomeMessage.vue';
+
+export default {
+  components: {
+    WelcomeMessage,
+  },
+};
+</script>
+
---
+++ src/browser/vue/WelcomeMessage.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  v-dialog(v-model="open")
+    v-card
+      v-card-title
+        span.headline {{title}}
+      v-card-text.pb-0
+        div(v-for="line in body") {{line}}
+        v-checkbox(label="次回から表示しない。", v-model="dontShowAgain")
+      v-card-actions
+        v-spacer
+        v-btn(primary, @click="open = false") 閉じる
+</template>
+
+<script>
+import config from '../config';
+
+const StorageKey = 'nekotaku/welco-memessage/hidden-id';
+
+export default {
+  data() {
+    return {
+      id: null,
+      title: null,
+      body: [],
+      ...config.welcomeMessage,
+
+      open: config.welcomeMessage && localStorage.getItem(StorageKey) !== config.welcomeMessage.id,
+      dontShowAgain: false,
+    };
+  },
+  watch: {
+    dontShowAgain(b) {
+      if (b) localStorage.setItem(StorageKey, this.id);
+      else localStorage.removeItem(StorageKey);
+    },
+  },
+};
+</script>
+
