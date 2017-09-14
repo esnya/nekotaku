340a3725136a287578eb3cec6bf81cef4ebd7a4d
ukatama <dev.ukatama@gmail.com>
2017-09-14 22:41:55 +0900

    Fix sound volume

--- src/browser/utilities/noticeSound.js
+++ src/browser/utilities/noticeSound.js
@@ -1,8 +1,8 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 
-const gain = context.createGain();
-gain.connect(context.destination);
+const gainNode = context.createGain();
+gainNode.connect(context.destination);
 
 function avoidTouch() {
   window.removeEventListener('touchstart', avoidTouch);
@@ -15,11 +15,13 @@ function avoidTouch() {
 }
 window.addEventListener('touchstart', avoidTouch);
 
-export default function playNoticeSound() {
+export default function playNoticeSound(gain: number = 0.25) {
+  gainNode.gain.value = gain;
+
   const oscillator = context.createOscillator();
   oscillator.type = 'sine';
   oscillator.frequency.value = 523.251;
-  oscillator.connect(gain);
+  oscillator.connect(gainNode);
   oscillator.start();
   setTimeout(() => {
     oscillator.stop();
