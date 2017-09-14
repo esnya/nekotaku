9877494306f45a95d72cbb31b76b8a977912ba48
ukatama <dev.ukatama@gmail.com>
2017-09-14 18:55:33 +0900

    Avoid AudioContext touch limitation

--- src/browser/utilities/noticeSound.js
+++ src/browser/utilities/noticeSound.js
@@ -1,11 +1,22 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+const context = new AudioContext();
 
-const gain = audioContext.createGain();
-gain.connect(audioContext.destination);
+const gain = context.createGain();
+gain.connect(context.destination);
+
+function avoidTouch() {
+  window.removeEventListener('touchstart', avoidTouch);
+
+  const buffer = context.createBuffer(1, 1, 22050);
+  const source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(0);
+}
+window.addEventListener('touchstart', avoidTouch);
 
 export default function playNoticeSound() {
-  const oscillator = audioContext.createOscillator();
+  const oscillator = context.createOscillator();
   oscillator.type = 'sine';
   oscillator.frequency.value = 523.251;
   oscillator.connect(gain);
