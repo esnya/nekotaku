6e8257947e2fba5f992be626b9981df6390cc2b8
ukatama <dev.ukatama@gmail.com>
2018-09-28 10:48:08 +0900

    Fix production build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -16,6 +16,7 @@ const plugins = isProduction ? [
     compress: {
       warnings: false,
     },
+    exclude: /node_modules\/set-value/,
   }),
   new webpack.LoaderOptionsPlugin({
     minimize: true,
